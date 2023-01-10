384b66af9709ce69bd0dc1fed773ddf0840a02ce
Syed Mohammad Yasir <smy329@gmail.com>
2023-01-11 00:13:48 +0600

    finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,10 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className={`flex md:flex-row flex-col ${styles.paddingX}`}
-    >
+      className={`flex md:flex-row flex-col`}>
       <div
-        className={`flex-1 ${styles.flexStart} flex-col px-6 sm:px-16 xl:px-0`}
-      >
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0`}>
+        
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
 
